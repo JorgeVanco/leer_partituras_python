b157af6f27c759c96ed33ce40770b2076992fa95
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2023-01-11 14:12:20 +0100

    Imagenes menu

--- app/pygame_funcs/ajustes.py
+++ app/pygame_funcs/ajustes.py
@@ -49,7 +49,7 @@ def return_to_default_settings(AJUSTES, AJUSTES_DEFAULT, menu, screen, complete_
 def draw_menu(menu, screen, AJUSTES, AJUSTES_DEFAULT, complete_path):
     menu.clear()
     menu.add.range_slider("Porcentaje para detectar una nota: "+str(AJUSTES.PORCENTAJE_DETECTAR_NOTA), default=AJUSTES.PORCENTAJE_DETECTAR_NOTA, range_values=(0, 1), increment=0.1, onchange = lambda value:set_porcentaje_detectar_nota(value, AJUSTES))
-    menu.add.range_slider("Porcentaje para diferenciar entre negras y blancas: "+str(AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA), default=AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA, range_values=(0, 1), increment=0.1, onchange = lambda value:set_porcentaje_diferenciar_negra_blanca(value, AJUSTES))
+    menu.add.range_slider("Porcentaje para diferenciar negras de blancas: "+str(AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA), default=AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA, range_values=(0, 1), increment=0.1, onchange = lambda value:set_porcentaje_diferenciar_negra_blanca(value, AJUSTES))
     menu.add.range_slider("Umbral negro: "+str(AJUSTES.UMBRAL_NEGRO), default=AJUSTES.UMBRAL_NEGRO, range_values=(0, 255), increment=1, onchange = lambda value:set_umbral(value, AJUSTES, menu, screen))
     menu.add.range_slider("Fracción mínima pixeles negros: "+str(AJUSTES.FRACCION_MINIMA_PIXELES_NEGROS) + " ", default=AJUSTES.FRACCION_MINIMA_PIXELES_NEGROS, range_values=(0, 1), increment=0.1, onchange = lambda value:set_fraccion(value, AJUSTES))
     menu.add.selector('Detectar corcheas :'+str(AJUSTES.DETECTAR_CORCHEAS), [('No', False), ('Sí', True)], default=AJUSTES.DETECTAR_CORCHEAS, onchange= lambda value, boolean: set_corcheas(value, boolean, AJUSTES))
@@ -63,15 +63,22 @@ def main_ajustes():
 
     complete_path:str = find_complete_path(__file__)
 
-    w:int = 1080
-    h:int = 700
+    w:int = 1024
+    h:int = 768
     screen = pygame.display.set_mode((w, h))
+
+
+    my_image = pygame_menu.baseimage.BaseImage(
+    image_path=complete_path + "app/pygame_funcs/imagenes_menu/escenario.png")
+
+    my_theme = pygame_menu.Theme(background_color=my_image, # transparent background
+                title_background_color=(20, 20, 20), widget_font=pygame_menu.font.FONT_OPEN_SANS_BOLD, widget_font_color = (255,255,255))
     
     AJUSTES_DEFAULT:Ajustes = Ajustes()
     AJUSTES:Ajustes = get_ajustes()
         
     menu = pygame_menu.Menu('Ajustes', w, h,
-                        theme=pygame_menu.themes.THEME_DARK)
+                        theme=my_theme)
 
     draw_menu(menu, screen, AJUSTES, AJUSTES_DEFAULT, complete_path)
 
--- app/pygame_funcs/menu.py
+++ app/pygame_funcs/menu.py
@@ -4,23 +4,27 @@ from pygame_funcs.main_pygame import main_pygame
 from lectura_partituras.main_lectura_partituras import main_lectura_partituras
 from pygame_funcs.ajustes import main_ajustes
 from musica.musica import main_musica
+from lectura_partituras.functions.functions import find_complete_path
 
 from Classes.Errors import ImageNotSelected, ErrorPentagramas, ErrorPath
 
 
 def main_menu():
     pygame.init()
-    COLORS = {"GRAY":(150, 150, 150), "RED":(255, 0, 0), "BG": (20,20,20)}
+
+    complete_path = find_complete_path(__file__)
+    bg = pygame.image.load(complete_path + "app/pygame_funcs/imagenes_menu/escenario.png")
 
     running = True
-    w = 760
-    h = 760
+    w = 1024
+    h = 768
     screen = pygame.display.set_mode((w, h))
 
     buttons_width: int = 400
     
     x:int = w // 2 - buttons_width//2
-    font = pygame.font.SysFont('Arxºial', 40)
+    height:int = 100
+    font = pygame.font.SysFont('microsoftjhengheimicrosoftjhengheiuibold', 40)
 
     clock = pygame.time.Clock()
 
@@ -29,19 +33,19 @@ def main_menu():
         pygame.display.set_caption("Menú Lectura Partituras")
         clock.tick(60)/1000.0
 
-        button_lectura = b.Button(x, 30, buttons_width, 100, 'LEER PARTITURA', font, main_lectura_partituras)
-        button_edit = b.Button(x, 140, buttons_width, 100, "EDITAR PARTITURA", font, main_pygame)
-        button_music = b.Button(x, 250, buttons_width, 100, "TOCAR MÚSICA", font, main_musica)
-        button_options = b.Button(x, 360, buttons_width, 100, 'AJUSTES', font, main_ajustes)
-        button_instructions = b.Button(x, 470, buttons_width, 100, 'INSTRUCCIONES', font, lambda: True)
-        button_quit = b.Button(x, 580, buttons_width, 100, 'QUIT', font, lambda: False)
+        button_lectura = b.Button(x, 50, buttons_width, height, 'LEER PARTITURA', font, main_lectura_partituras)
+        button_edit = b.Button(x, 160, buttons_width, height, "EDITAR PARTITURA", font, main_pygame)
+        button_music = b.Button(x, 270, buttons_width, height, "TOCAR MÚSICA", font, main_musica)
+        button_options = b.Button(x, 380, buttons_width, height, 'AJUSTES', font, main_ajustes)
+        button_instructions = b.Button(x, 490, buttons_width, height, 'INSTRUCCIONES', font, lambda: True)
+        button_quit = b.Button(x, 600, buttons_width, height, 'QUIT', font, lambda: False)
 
         buttons = [button_options, button_lectura, button_music, button_edit, button_instructions, button_quit]
 
 
         screen = pygame.display.set_mode((w, h))
-        screen.fill(COLORS["BG"])
-        
+        screen.blit(bg, (0, 0))
+
         for button in buttons:
             try:
                 running = button.process(screen)

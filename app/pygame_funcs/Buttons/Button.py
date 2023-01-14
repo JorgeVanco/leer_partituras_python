9a71716b6dffdc692008f4aad0dd21b7717866f6
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2023-01-14 17:46:45 +0100

    Comentado Button.py

--- app/pygame_funcs/Buttons/Button.py
+++ app/pygame_funcs/Buttons/Button.py
@@ -20,6 +20,12 @@ class Button:
         }
 
     def render(self, screen) -> None:
+        """
+        Dibuja el botón en la pantalla
+
+        Args:
+            screen: La superficie de pygame sobre la que dibujar el botón
+        """
         self.button_surface.blit(self.button_surf, [
             self.button_rect.width/2 - self.button_surf.get_rect().width/2,
             self.button_rect.height/2 - self.button_surf.get_rect().height/2
@@ -28,11 +34,24 @@ class Button:
 
     
     def process(self, screen) -> bool:
+        """
+        Comprueba si el cursor está sobre el botón y si ha sido pulsado. 
+        Ejecuta la función asignada al botón.
+
+        Args:
+            screen: La superficie de pygame sobre la que dibujar el botón
+        
+        Returns: 
+            running: Si el código debe seguir corriendo o no
+        """
         running:bool = True
         mousePos = pygame.mouse.get_pos()
+        
         self.button_surface.fill(self.fill_colors['normal'])
+        
         if self.button_rect.collidepoint(mousePos):
             self.button_surface.fill(self.fill_colors['hover'])
+            
             if pygame.mouse.get_pressed(num_buttons=3)[0]:
                 self.button_surface.fill(self.fill_colors['pressed'])
                 try:
@@ -47,7 +66,7 @@ class Button:
                     raise FileNotFoundError(e)
                 except ErrorPath as e:
                     raise ErrorPath(e)
-
+        
         self.render(screen)
         
         return running

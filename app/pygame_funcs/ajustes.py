2ed17d3d291037c0788dc2005e6eae91d44c3496
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2023-01-14 23:12:39 +0100

    Comentado ajustes.py

--- app/pygame_funcs/ajustes.py
+++ app/pygame_funcs/ajustes.py
@@ -7,34 +7,41 @@ import os
 from lectura_partituras.functions.functions import find_complete_path, get_ajustes
 
 
-def set_corcheas(value, boolean, AJUSTES:Ajustes) -> None:
-    AJUSTES.DETECTAR_CORCHEAS = boolean
+def set_ajustes(value, ajuste:str, AJUSTES:Ajustes) -> None:
+    """
+    Asigna un nuevo valor al atributo del objeto de los ajustes
 
+    Args:
+        value (any): El nuevo valor
+        ajuste (str): El nombre del atributo que se desea cambiar
+        AJUSTES (Ajustes): El objeto de los ajustes
+    """
+    if ajuste == "detectar_corcheas":
+        AJUSTES.DETECTAR_CORCHEAS = value
+    elif ajuste == "cambiar_size":
+        AJUSTES.CAMBIAR_SIZE = value
+    elif ajuste == "umbral_negro":
+        AJUSTES.UMBRAL_NEGRO = int(value)
+    elif ajuste == "porcentaje_detectar_nota":
+        AJUSTES.PORCENTAJE_DETECTAR_NOTA = float(np.round(value, 3))
+    elif ajuste == "porcentaje_diferenciar_negra_blanca":
+        AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA = float(np.round(value, 3))
+    elif ajuste == "fraccion_minima_pixeles_negros":
+        AJUSTES.FRACCION_MINIMA_PIXELES_NEGROS = float(np.round(value, 3))
+    elif ajuste == "tempo_partitura":
+        AJUSTES.TEMPO_PARTITURA = int(value)
 
-def set_resize(value, boolean, AJUSTES:Ajustes) -> None:
-    AJUSTES.CAMBIAR_SIZE = boolean
+def save(AJUSTES:Ajustes, AJUSTES_DEFAULT:Ajustes, menu:pygame_menu.Menu, screen, complete_path:str) -> None:
+    """
+    Guarda los valores de los ajustes en un fichero de bytes
 
-
-def set_umbral(value, AJUSTES:Ajustes) -> None:
-    AJUSTES.UMBRAL_NEGRO = int(value)
-
-
-def set_porcentaje_detectar_nota(value, AJUSTES:Ajustes) -> None:
-    AJUSTES.PORCENTAJE_DETECTAR_NOTA = np.round(value, 3)
-
-
-def set_porcentaje_diferenciar_negra_blanca(value, AJUSTES:Ajustes) -> None:
-    AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA = np.round(value, 3)
-
-
-def set_fraccion(value, AJUSTES:Ajustes) -> None:
-    AJUSTES.FRACCION_MINIMA_PIXELES_NEGROS = np.round(value, 3)
-
-def set_tempo_partitura(value, AJUSTES:Ajustes) -> None:
-    AJUSTES.TEMPO_PARTITURA = int(value)
-
-
-def save(AJUSTES, AJUSTES_DEFAULT, menu, screen, complete_path) -> None:
+    Args:
+        AJUSTES (Ajustes): El objeto de los ajustes
+        AJUSTES_DEFAULT (Ajustes): El objeto de los ajustes con los valores predeterminados
+        menu (pygame_menu.Menu): El objeto menu de pygame_menu
+        screen: La pantalla de pygame
+        complete_path (str): La ruta absoluta hasta el directorio app
+    """
     saved: bool = False
     while not saved:
         try:
@@ -47,32 +54,59 @@ def save(AJUSTES, AJUSTES_DEFAULT, menu, screen, complete_path) -> None:
     draw_menu(menu, screen, AJUSTES, AJUSTES_DEFAULT, complete_path)
 
 
-def quit(menu) -> None:
+def quit(menu:pygame_menu.Menu) -> None:
+    """
+    Cierra la ventana de ajustes
+
+    Args:
+        menu (pygame_menu.Menu): El objeto menu de pygame_menu
+    """
     menu.disable()
 
 
-def return_to_default_settings(AJUSTES:Ajustes, AJUSTES_DEFAULT:Ajustes, menu, screen, complete_path:str) -> None:
+def return_to_default_settings(AJUSTES:Ajustes, AJUSTES_DEFAULT:Ajustes, menu:pygame_menu.Menu, screen, complete_path:str) -> None:
+    """
+    Vuelve los valores de los ajustes a los valores por defecto
+
+    Args:
+        AJUSTES (Ajustes): El objeto de los ajustes
+        AJUSTES_DEFAULT (Ajustes): El objeto de los ajustes con los valores predeterminados
+        menu (pygame_menu.Menu): El objeto menu de pygame_menu
+        screen: La pantalla de pygame
+        complete_path (str): La ruta absoluta hasta el directorio app
+    """
     for attribute in AJUSTES.__dict__:
         setattr(AJUSTES, attribute, getattr(AJUSTES_DEFAULT, attribute))
+    save(AJUSTES, AJUSTES_DEFAULT, menu, screen, complete_path)
     draw_menu(menu, screen, AJUSTES, AJUSTES_DEFAULT, complete_path)
 
 
-def draw_menu(menu, screen, AJUSTES:Ajustes, AJUSTES_DEFAULT:Ajustes, complete_path:str) -> bool:
+def draw_menu(menu, screen, AJUSTES:Ajustes, AJUSTES_DEFAULT:Ajustes, complete_path:str):
+    """
+    Dibuja todas las opciones de los ajustes en la pantalla
+
+    Args:
+        menu (pygame_menu.Menu): El objeto menu de pygame_menu
+        screen: La pantalla de pygame
+        AJUSTES (Ajustes): El objeto de los ajustes
+        AJUSTES_DEFAULT (Ajustes): El objeto de los ajustes con los valores predeterminados
+        complete_path (str): La ruta absoluta hasta el directorio app
+    """
     menu.clear()
     menu.add.range_slider("Porcentaje para detectar una nota: "+str(AJUSTES.PORCENTAJE_DETECTAR_NOTA), default=AJUSTES.PORCENTAJE_DETECTAR_NOTA,
-                            range_values=(0, 1), increment=0.1, onchange=lambda value: set_porcentaje_detectar_nota(value, AJUSTES))
+                            range_values=(0, 1), increment=0.05, onchange = lambda value: set_ajustes(value, "porcentaje_detectar_nota", AJUSTES))
     menu.add.range_slider("Porcentaje para diferenciar negras de blancas: "+str(AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA), default=AJUSTES.PORCENTAJE_DIFERENCIAR_NEGRA_BLANCA,
-                            range_values=(0, 1), increment=0.1, onchange=lambda value: set_porcentaje_diferenciar_negra_blanca(value, AJUSTES))
+                            range_values=(0, 1), increment=0.05, onchange = lambda value: set_ajustes(value, "porcentaje_diferenciar_negra_blanca", AJUSTES))
     menu.add.range_slider("Umbral negro: "+str(AJUSTES.UMBRAL_NEGRO), default=AJUSTES.UMBRAL_NEGRO,
-                            range_values=(0, 255), increment=1, onchange=lambda value: set_umbral(value, AJUSTES, menu, screen))
+                            range_values=(0, 255), increment=1, onchange=lambda value: set_ajustes(value, "umbral_negro", AJUSTES))
     menu.add.range_slider("Fracción mínima pixeles negros: "+str(AJUSTES.FRACCION_MINIMA_PIXELES_NEGROS) + " ",
-                            default=AJUSTES.FRACCION_MINIMA_PIXELES_NEGROS, range_values=(0, 1), increment=0.1, onchange=lambda value: set_fraccion(value, AJUSTES))
+                            default=AJUSTES.FRACCION_MINIMA_PIXELES_NEGROS, range_values=(0, 1), increment=0.05, onchange=lambda value: set_ajustes(value, "fraccion_minima_pixeles_negros", AJUSTES))
     menu.add.selector('Detectar corcheas :'+str(AJUSTES.DETECTAR_CORCHEAS),
-                        [('No', False), ('Sí', True)], default=AJUSTES.DETECTAR_CORCHEAS, onchange=lambda value, boolean: set_corcheas(value, boolean, AJUSTES))
+                        [('No', False), ('Sí', True)], default=AJUSTES.DETECTAR_CORCHEAS, onchange=lambda value, boolean: set_ajustes(boolean,"detectar_corcheas", AJUSTES))
     menu.add.selector('Cambiar tamaño partitura :'+str(AJUSTES.CAMBIAR_SIZE),
-                        [('No', False), ('Sí', True)], default=AJUSTES.CAMBIAR_SIZE, onchange=lambda value, boolean: set_resize(value, boolean, AJUSTES))
+                        [('No', False), ('Sí', True)], default=AJUSTES.CAMBIAR_SIZE, onchange=lambda value, boolean: set_ajustes(boolean, "cambiar_size", AJUSTES))
     menu.add.range_slider("Tempo partitura: "+str(AJUSTES.TEMPO_PARTITURA), default=AJUSTES.TEMPO_PARTITURA,
-                            range_values=(40, 255), increment=1, onchange=lambda value: set_tempo_partitura(value, AJUSTES))
+                            range_values=(40, 255), increment=1, onchange=lambda value: set_ajustes(value, "tempo_partitura", AJUSTES))
     menu.add.button("Default settings", lambda: return_to_default_settings(
         AJUSTES, AJUSTES_DEFAULT, menu, screen, complete_path))
     
@@ -80,12 +114,13 @@ def draw_menu(menu, screen, AJUSTES:Ajustes, AJUSTES_DEFAULT:Ajustes, complete_p
         AJUSTES, AJUSTES_DEFAULT, menu, screen, complete_path))
 
     menu.add.button('Return', lambda: quit(menu))
-    
 
 
 
-def main_ajustes():
-
+def main_ajustes() -> bool:
+    """
+    La lógica del menú de ajustes
+    """
     complete_path: str = find_complete_path(__file__)
 
     w: int = 1024
@@ -104,9 +139,6 @@ def main_ajustes():
     menu = pygame_menu.Menu('Ajustes', w, h,
                             theme=my_theme)
     
-
     draw_menu(menu, screen, AJUSTES, AJUSTES_DEFAULT, complete_path)
 
     menu.mainloop(screen)
-
-    return True

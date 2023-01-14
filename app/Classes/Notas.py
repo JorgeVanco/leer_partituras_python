6944ac48bd815b889f079e85db8504580752d2ac
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2023-01-14 12:47:23 +0100

    Comentado Notas.py

--- app/Classes/Notas.py
+++ app/Classes/Notas.py
@@ -21,11 +21,20 @@ class Partitura:
 
         self.asignar_notas_a_pentagrama()
 
-    def borrar_nota(self, index):
+    def borrar_nota(self, index:int) -> None:
+        """
+        Elimina la nota de la lista de notas de la partitura y vuelve a asignar las notas a los pentagramas
+
+        Args:
+            index (int): El índice de la nota en la lista de notas de la partitura
+        """
         self.notas = self.notas[:index] + self.notas[index + 1:]
         self.asignar_notas_a_pentagrama()
 
-    def asignar_notas_a_pentagrama(self):
+    def asignar_notas_a_pentagrama(self) -> None:
+        """
+            Asigna las notas pertenecientes a cada pentagrama a su pentagrama correspondiente
+        """
         index_pentagrama:int = 0
         index_notas:int = 0
 
@@ -42,14 +51,28 @@ class Partitura:
             index_pentagrama += 1
 
 
-    def crear_lista_pentagramas(self):
+    def crear_lista_pentagramas(self) -> list:
+        """
+        Crea una lista de pentagramas con las notas vacías
+        """
         return [Pentagrama([], posiciones) for posiciones in self.posiciones_pentagramas]
 
 
-    def find_index_nota_at_position(self, pos):
+    def find_index_nota_at_position(self, pos:list) -> int:
+        """
+        Encuentra el índice de la nota que se encuentra en la posicion dada
+
+        Args:
+            pos (list): La posicion
+
+        Returns:
+            (int): El índice de la nota, o None si no existe
+        """
         for i, nota in enumerate(self.notas):
             if pos[0] <= nota.rectangulo[3] and pos[0] >= nota.rectangulo[2] and pos[1] <= nota.rectangulo[1] and pos[1] >= nota.rectangulo[0]:
                 return i
+        
+        return None
 
 class Pentagrama:
     def __init__(self, notas, posiciones) -> None:
@@ -57,9 +80,20 @@ class Pentagrama:
         self.posiciones = posiciones
         self.asignar_pentagrama_a_nota()
 
-    def asignar_pentagrama_a_nota(self):
+    def asignar_pentagrama_a_nota(self) -> None:
+        """
+        Asigna el pentagrama a cada nota de la lista de notas de este pentagrama
+        """
         for nota in self.notas:
             nota.pentagrama = self
     
-    def cambiar_nota(self, nota, atributo, valor):
+    def cambiar_nota(self, nota:Nota, atributo:str, valor) -> None:
+        """
+        Cambia el valor de un atributo de una nota
+
+        Args:
+            nota (Nota): La nota a la que cambiarle el valor del atributo
+            atributo (str): El nombre del atributo a cambiar
+            valor (Any): El nuevo valor
+        """
         setattr(nota, atributo, valor)
